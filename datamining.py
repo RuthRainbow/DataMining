589cb3742024468e7f7d7dcc7c7fa8302c1695cc
Ruth King <ruth.king1991@gmail.com>
2014-04-28 10:54:11 +0100

    Use scilearn classifiers and feature selection

--- datamining.py
+++ datamining.py
@@ -8,72 +8,135 @@ from nltk.chunk import ne_chunk
 from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
 from nltk.stem.wordnet import WordNetLemmatizer
+import numpy
 import regex
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_selection import SelectKBest, chi2
+from sklearn.svm import LinearSVC
+from sklearn.naive_bayes import MultinomialNB
+from sklearn import metrics
 from textblob import TextBlob, Word
 from textblob.classifiers import NaiveBayesClassifier
 
 def main():
+  # Whether we are using scilearn for features or our own custom.
+  scilearn = True
+
   soups = []
   raw_texts = []
   texts = []
   topics = []
-  lewis = []
   training_set = []
+  training_data = []
+  training_topics = []
   test_set = []
+  test_data = []
+  test_topics = []
 
   interested_topics = {'corn', 'earn', 'acquisitions', 'money-fx', 'grain', 'crude', 'trade', 'interest', 'ship', 'wheat'}
 
-  #for i in range(0, 22):
-  for i in range(0, 1):
+  for i in range(0, 22):
     num = str(i)
     if (i < 10): 
       num = "0" + str(i);
-    newSoup = BeautifulSoup(open("/home/ruth/uni/dm/ass/data/reut2-0"+num+".sgm"))
-    soups.append(newSoup)
+    #if i == 0 or i == 21:
+    if True:
+      newSoup = BeautifulSoup(open("/home/ruth/uni/dm/ass/data/reut2-0"+num+".sgm"))
+      soups.append(newSoup)
+
+  print 'loaded %d soups' % len(soups)
     
   for i in range(0, len(soups)):
     thisSoup = soups[i]
     theseBodies = thisSoup.findAll('text')
     theseTopics = thisSoup.findAll('topics')
     reuters = thisSoup.findAll('reuters')
+    lewis = []
     for reut in reuters:
       lewis.append(reut.get('lewissplit'))
-    print len(lewis)
-    print len(theseBodies)
-    print len(theseTopics)
     
-    #for i in range(1, min(len(theseBodies), len(theseTopics))):
-    for i in range(1, 30):
-      topic = theseTopics[i].text
+    for j in range(1, len(theseBodies)):
+      topic = theseTopics[j].text
       if topic in interested_topics:
-        body = theseBodies[i].text
-        #print body
-        texts.append(clean_body(body))
-        if lewis[i] == 'TRAIN':
-          training_set.append([clean_body(body), topic])
-        else:
-          test_set.append([clean_body(body), topic])
+        body = theseBodies[j].text
+        texts.append(clean_body(body, scilearn))
+        if lewis[j] == 'TRAIN':
+          training_set.append([clean_body(body, scilearn), topic])
+          training_data.append(clean_body(body, scilearn))
+          training_topics.append(topic)
+        elif lewis[j] == 'TEST':
+          test_set.append([clean_body(body, scilearn), topic])
+          test_data.append(clean_body(body, scilearn))
+          test_topics.append(topic)
         raw_texts.append(body)
-        #print clean_body(body)
+        #print clean_body(body, scilearn)
         topics.append(topic)
 
-  print len(texts)
-  print len(topics)
-  print len(soups)
+  # FOR TESTING
+  if len(test_data) == 0:
+    print 'Making artificial test data'
+    text = 'i am a test data, hello!'
+    topic = 'earn'
+    test_set.append([clean_body(body, scilearn), topic])
+    test_data.append(clean_body(body, scilearn))
+    test_topics.append(topic)
 
-  # Feature selection methods:
-  #bag_of_words(texts)
-  #print calc_tf_idf(texts, 10)
+  if scilearn:
+    vect = TfidfVectorizer(strip_accents = 'unicode',
+                           sublinear_tf = True,
+                           max_df = 0.5,
+                           stop_words = 'english')
+    featured_train = vect.fit_transform(training_data)
+    print 'Training: n_samples: %d, n_features: %d' % featured_train.shape
+    featured_test = vect.transform(test_data)
+    print 'Test: n_samples: %d, n_features: %d' % featured_test.shape
+    print 'Fininshed TfIdf vectoriser'
 
-  # Classification
-  NB = NaiveBayesClassifier(training_set)
-  #print test_set[0]
-  #print NB.classify((test_set[0])[0])
-  #print test_set[(test_set[0])[1]]
-  print NB.classify(['things', 'profit'])
+    chi = SelectKBest(chi2, 10)
+    featured_train = chi.fit_transform(featured_train, training_topics)
+    featured_test = chi.transform(featured_test)
+    print 'Finished chi^2'
 
-  print NB.classify(training_set[0][0])
-  print training_set[0][1]
+    feature_names = numpy.asarray(vect.get_feature_names())
+    print feature_names
+
+
+    classify(MultinomialNB(alpha=0.1),
+             featured_train,
+             training_topics,
+             featured_test,
+             test_topics)
+
+    classify(LinearSVC(), featured_train, training_topics, featured_test, test_topics)
+
+  if not scilearn:
+    # Feature selection methods:
+    #bag_of_words(texts)
+    #print calc_tf_idf(texts, 10)
+
+    # Classification
+    NB = NaiveBayesClassifier(training_set)
+    print test_set[0]
+    print NB.classify((test_set[0])[0])
+    print test_set[(test_set[0])[1]]
+    #print NB.classify(['things', 'profit'])
+
+    #print NB.classify(training_set[0][0])
+    #print training_set[0][1]
+
+    print NB.accuracy(test_set)
+    print NB.show_informative_features(50)
+
+
+def classify(classifier, training_data, training_topics, test_data, test_topics):
+  print 'training: ' + str(classifier)
+  classifier.fit(training_data, training_topics)
+  print 'testing'
+  pred = classifier.predict(test_data)
+  score = metrics.f1_score(test_topics, pred)
+  print 'f1 score: %f' % score
+  print 'report:'
+  #print metrics.classification_report(test_data, pred)
 
 
 def calc_tf_idf(texts, x):
@@ -129,7 +192,10 @@ def bag_of_words(texts):
 
 
 # Method to carry out pre-processing and cleaning of bodies
-def clean_body(body):
+def clean_body(body, scilearn):
+  # If using scilearn for feature extraction just remove whitespace
+  if scilearn:
+    return ' '.join(body.split())
   # Remove title and date - we only want the text. Join also removes excess whitespace
   body = ' '.join([body.split('-')[i] for i in range(1, len(body.split('-')))])
   # Tokenise & Remove the final word "reuter"
